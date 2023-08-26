879e2bae31005619d9247a3d4171e477411e08c4
Tkakishak <tkakishakd@mail.ru>
2023-08-26 23:37:06 +0300

    Install

---
+++ src/main/java/com/example/HW2108/Hw2108Application.java
@@ -0,0 +1,13 @@
+package com.example.HW2108;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2108Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw2108Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HW2108/Hw2108ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HW2108;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2108ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
